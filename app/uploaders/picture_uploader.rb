fcf2b848c3376101c2249925c61bed9faf09bb52
Thơm Dứa <chuthom97@gmail.com>
2017-09-17 08:26:40 +0000

    adssssd

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -3,10 +3,10 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-  # version :thumb do
-  #   process :resize_to_fill => [150, 150]
-  # end
+  include CarrierWave::MiniMagick
+  version :thumb do
+    process :resize_to_fill => [300, 300]
+  end
   
   # version :small_thumb, from_version: :thumb do
   #   process resize_to_fill: [100, 100]
